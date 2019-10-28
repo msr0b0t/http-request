2352df06b8631a1465e22ad5749b92f7391efde0
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-10-28 12:27:16 +0200

    Meta tweaks

--- test/test.js
+++ test/test.js
@@ -1,40 +1,36 @@
 /* eslint-env node, mocha */
-const assert = require('assert');
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
-const article = require('..');
+const assert = require("assert");
+const mongoose = require("mongoose");
+const article = require("..");
 
-mongoose.connect('mongodb://localhost:27017/http-request-db');
+// mongoose.connect is a side-effect of require('..'). No need to do it again
 
-const Article = require('../models/article');
+const Article = require("../models/article");
 
-describe('getArticle', () => {
-	before(done => {
-		Article.remove({}).exec().then(() => {
-			done();
-		});
+describe("getArticle", () => {
+	before(async () => {
+		await Article.deleteMany({}).exec();
 	});
-	after(() => {
-		mongoose.connection.close();
+
+	after(async () => {
+		await mongoose.disconnect();
 		process.exit(0);
 	});
-	it('should push doi to db', done => {
-		const url = 'https://api.crossref.org/works/10.1017/cbo9781139058605.030';
-		article.getArticle(url).then(result => {
-			assert.notEqual(result._id, undefined, 'Result id should not be undefined');
-			done();
-		});
+
+	it("should push doi to db", async () => {
+		const url = "https://api.crossref.org/works/10.1017/cbo9781139058605.030";
+		const result = await article.getArticle(url);
+		assert.notEqual(result._id, undefined, "Result id should not be undefined");
 	}).timeout(10000);
-	it('should push two dois to db', done => {
-		const url1 = 'https://api.crossref.org/works/10.1017/cbo9781139058605.030';
-		const url2 = 'https://api.crossref.org/journals/1549-7712';
+
+	it("should push two dois to db", async () => {
+		const url1 = "https://api.crossref.org/works/10.1017/cbo9781139058605.030";
+		const url2 = "https://api.crossref.org/journals/1549-7712";
 		const promise1 = article.getArticle(url1);
 		const promise2 = article.getArticle(url2);
-		bluebird.all([promise1, promise2]).then(result => {
-			const articleObject1 = result[0];
-			const articleObject2 = result[1];
-			assert.notEqual([articleObject1._id, articleObject2._id], [undefined, undefined], 'Result ids should not be undefined');
-			done();
-		});
+		const result = await Promise.all([promise1, promise2]);
+		const articleObject1 = result[0];
+		const articleObject2 = result[1];
+		assert.notEqual([articleObject1._id, articleObject2._id], [undefined, undefined], "Result ids should not be undefined");
 	}).timeout(15000);
 });
